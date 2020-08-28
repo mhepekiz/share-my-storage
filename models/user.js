f700229a685391e8817c09b375a82ac1898f4494
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-08-27 22:00:12 -0500

    User model added

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: String,
+    phone: String,
+    email: String,
+    location: String,
+    userrole: {
+        type: String,
+        Enum: ['user', 'admin']
+    },
+    userstatus: {
+        type: String,
+        Enum: ['active', 'passive']
+    },
+    googleId: String
+},
+
+    {
+      timestamps: true
+    });
+    
+  
+  
+  module.exports = mongoose.model('Flight', flightSchema);
+  
+  
