8cc50cf0eecb1b0ea9e3645cfc06f6dacc5ce993
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-08-27 21:51:26 -0500

    Bootstrap and header/footer added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
+
 require('dotenv').config();
 require('./config/database');
 
@@ -22,7 +24,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('_method'));
+ 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
