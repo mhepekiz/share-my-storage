3bcb9b5b3d9114cdd0bf7916b0274918e4151597
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-09-02 22:49:05 -0500

    Admin functions done

--- controllers/admins.js
+++ controllers/admins.js
@@ -5,7 +5,11 @@ let Contact = require('../models/contact');
 
 
 module.exports = {
-  index
+  index,
+  listStorages,
+  listUsers,
+  deleteStorage,
+  deleteUser
 }
   
 
@@ -13,9 +17,36 @@ function index(req, res) {
   Storage.count({}, function (err, count) {
     console.log(count);
   });
-    Storage.find({}, function(err, storages) {
-         User.find({}, function(err, users) {
-        res.render('./admin/index', { title: 'Share My Storage Admin', subtitle: 'All Storages', storages, users });
+            User.find({}, function(err, users) {
+        res.render('./admin/index', { title: 'Share My Storage Admin', subtitle: 'All Storages',  users });
       }).sort( { timestamp: 1 } );
-    }).sort( { timestamp: 1 } );
     }
+
+
+    function listStorages(req, res) {
+        Storage.find({}, function(err, storages) {
+          res.render('./admin/adminstorages', { title: 'Share My Storage Admin', subtitle: 'All Storages', storages})
+        })
+    }
+    function listUsers(req, res) {
+      User.find({}, function(err, users) {
+        res.render('./admin/adminusers', { title: 'Share My Storage Admin', subtitle: 'All Users', users})
+      })
+  }
+
+
+function deleteStorage(req, res) {
+  Storage.findByIdAndDelete(req.params.id, function (err, storage) {
+      res.redirect(`/admins/adminstorages`);
+  });
+}
+
+
+
+function deleteUser(req, res) {
+  User.findByIdAndDelete(req.params.id, function (err, user) {
+    Storage.deleteMany({ "userID": user.id }, function (err, storage) {
+      res.redirect(`/admins/adminusers`);
+  });
+});
+}
--- routes/admins.js
+++ routes/admins.js
@@ -3,32 +3,35 @@ let passport = require('passport');
 let adminCtrl = require('../controllers/admins');
 
 router.get('/', adminCtrl.index);
-
+router.get('/adminstorages', adminCtrl.listStorages);
+router.get('/adminusers', adminCtrl.listUsers);
+router.delete('/:id', adminCtrl.deleteStorage);
+router.delete('/:id/user', adminCtrl.deleteUser);
 
   
 
 
-// // Google OAuth login route
-// router.get('/auth/google', passport.authenticate(
-//     'google',
-//     { scope: ['profile', 'email'] }
-//   ));
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+    'google',
+    { scope: ['profile', 'email'] }
+  ));
   
   
-//   // Google OAuth callback route
-//   router.get('/oauth2callback', passport.authenticate(
-//     'google',
-//     {
-//       successRedirect : '/',
-//       failureRedirect : '/'
-//     }
-//   ));
+  // Google OAuth callback route
+  router.get('/oauth2callback', passport.authenticate(
+    'google',
+    {
+      successRedirect : '/',
+      failureRedirect : '/'
+    }
+  ));
   
-//   // OAuth logout route
-//   router.get('/logout', function(req, res){
-//     req.logout();
-//     res.redirect('/');
-//   });
+  // OAuth logout route
+  router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
 
 
   
